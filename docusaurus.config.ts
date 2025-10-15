01668af7487405d936844535949832d480d7a609
Jamie Duncan <jamie.e.duncan@gmail.com>
2025-10-15 16:37:23 +0000

    fixing doc bug

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   organizationName: 'BrandCast-Signage',
   projectName: 'familycast-docs',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   i18n: {
